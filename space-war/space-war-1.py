058653995b02a237135f574859b09069ac53f577
oschre7741 <31477280+oschre7741@users.noreply.github.com>
2018-05-01 09:19:51 -0400

    Update space-war-1.py

--- space-war/space-war-1.py
+++ space-war/space-war-1.py
@@ -26,14 +26,22 @@ BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
 
+# Fonts
+FONT_SM = pygame.font.Font(None, 24)
+FONT_MD = pygame.font.Font(None, 32)
+FONT_LG = pygame.font.Font(None, 64)
+FONT_XL = pygame.font.Font(None, 96)
+
 # Images
 ship_img = pygame.image.load('images/link.png')
 laser_img = pygame.image.load('images/arrow.png')
 mob_img = pygame.image.load('images/mob.png')
 bomb_img = pygame.image.load('images/bomb.png')
 background = pygame.image.load('images/background.png')
+background2 = pygame.image.load('images/background2.png')
 startscreen = pygame.image.load('images/startscreen.png')
 game_over = pygame.image.load('images/game_over.png')
+win_img = pygame.image.load('images/win.png')
 
 # Sounds
 enemy_hit = pygame.mixer.Sound('sounds/enemy_hit.wav')
@@ -55,12 +63,11 @@ def start_music(music):
 # Stages
 START = 0
 PLAYING = 1
-END = 2
+LOSE = 2
+WIN = 3
 
 def setup():
     global x, y, image, lasers, bombs, mobs, player, ship, fleet, stage
-    
-    stage = START
     
     # Game classes
     class Ship(pygame.sprite.Sprite):
@@ -78,9 +85,13 @@ def setup():
 
         def move_left(self):
             self.rect.x -= self.speed
+            if self.rect.x <= 0:
+                self.rect.x = 0
             
         def move_right(self):
             self.rect.x += self.speed
+            if self.rect.x >= 928:
+                self.rect.x = 928
 
         def shoot(self):
             laser = Laser(laser_img)
@@ -101,7 +112,6 @@ def setup():
                 self.shield = 0
 
             if self.shield == 0:
-                stage = END
                 pygame.mixer.music.stop()
                 link_death.play()
                 self.kill()
@@ -140,7 +150,7 @@ def setup():
             bomb.rect.centery = self.rect.bottom
             bombs.add(bomb)
 
-        def update(self, lasers):
+        def update(self, lasers, player):
             hit_list = pygame.sprite.spritecollide(self, lasers, True, pygame.sprite.collide_mask)
 
             for hit in hit_list:
@@ -149,6 +159,7 @@ def setup():
                 
             if len(hit_list) > 0 or self.shield == 0:
                 enemy_die.play()
+                player.score += 1
                 self.kill()
 
 
@@ -217,23 +228,55 @@ def setup():
         
     # Make game objects
     ship = Ship(450, 465, ship_img)
-    mob1 = Mob(128, 64, mob_img)
-    mob2 = Mob(256, 64, mob_img)
-    mob3 = Mob(384, 64, mob_img)
+
+    mob1 = Mob(200, 64, mob_img)
+    mob2 = Mob(400, 64, mob_img)
+    mob3 = Mob(600, 64, mob_img)
+    mob4 = Mob(800, 64, mob_img)
+    mob5 = Mob(300, 14, mob_img)
+    mob6 = Mob(500, 14, mob_img)
+    mob7 = Mob(700, 14, mob_img)
+
+    mob8 = Mob(200, 64, mob_img)
+    mob9 = Mob(400, 64, mob_img)
+    mob10 = Mob(600, 64, mob_img)
+    mob11 = Mob(800, 64, mob_img)
+    mob12 = Mob(300, 14, mob_img)
+    mob13 = Mob(500, 14, mob_img)
+    mob14 = Mob(700, 14, mob_img)
 
     # Make sprite groups
     player = pygame.sprite.GroupSingle()
     player.add(ship)
+    player.score = 0
+    player.level = 1
 
     lasers = pygame.sprite.Group()
 
     mobs = pygame.sprite.Group()
-    mobs.add(mob1, mob2, mob3)
+    mobs.add(mob1, mob2, mob3, mob4, mob5, mob6, mob7)
+    if player.level == 2:
+        mobs = pygame.sprite.Group()
+        mobs.add(mob8, mob9, mob10, mob11, mob12, mob13, mob14)
 
     bombs = pygame.sprite.Group()
 
 
     fleet = Fleet(mobs)
+
+# Set stage
+    stage = START
+    
+# Game helper functions
+def show_title_screen():
+    screen.blit(startscreen, [0, 0])
+
+def show_stats(player):
+    score_text = FONT_MD.render('Score: ' + str(player.score), 1, WHITE)
+    screen.blit(score_text, [32, 32])
+
+    score_text = FONT_MD.render('Level: ' + str(player.level), 1, WHITE)
+    screen.blit(score_text, [32, 64])
 
 # Game loop
 setup()
@@ -245,6 +288,11 @@ if stage == START:
 if stage == PLAYING:
     current_music = 1
     pygame.mixer.music.play(-1)
+
+if stage == LOSE:
+    current_music = 2
+    pygame.mixer.music.play(-1)
+    
 done = False
 
 while not done:
@@ -253,41 +301,56 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                ship.shoot()
-
-        if stage == START:
-            if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                pygame.quit()
+                
+            if stage == START:
                 if event.key == pygame.K_KP_ENTER:
                     stage = PLAYING
-                    currrent_music = (current_music + 1) % len(music)
+                    current_music = (current_music + 1) % len(music)
                     start_music(music[current_music])
-                    
-        elif stage == PLAYING:            
-            pressed = pygame.key.get_pressed()
+            if event.key == pygame.K_SPACE and stage == PLAYING:
+                ship.shoot()
+                                  
+    if stage == PLAYING:
+        pressed = pygame.key.get_pressed()
 
-            if pressed[pygame.K_LEFT]:
-                ship.move_left()
-            elif pressed[pygame.K_RIGHT]:
-                ship.move_right()
+        if pressed[pygame.K_LEFT]:
+            ship.move_left()
+        elif pressed[pygame.K_RIGHT]:
+            ship.move_right()
+        
+    if stage == LOSE:
+        current_music = (current_music + 2) % len(music)
+        start_music(music[current_music])
 
-        elif stage == END:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    setup()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                setup()
+
+    if stage == WIN:    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                setup()
     
     # Game logic (Check for collisions, update points, etc.)
     if stage == PLAYING:
         player.update(bombs, mobs)
+        if len(player) == 0:
+            stage = LOSE
+            
         lasers.update()
-        mobs.update(lasers)
+        mobs.update(lasers, player)
+        if len(mobs) == 0:
+            player.level = 2
+            
         bombs.update()
         fleet.update()
 
         
     # Drawing code (Describe the picture. It isn't actually drawn yet.)
     if stage == START:
-        screen.blit(startscreen, (0, 0))
+        show_title_screen()
 
     if stage == PLAYING:
         screen.blit(background, (0, 0))
@@ -295,9 +358,23 @@ while not done:
         player.draw(screen)
         bombs.draw(screen)
         mobs.draw(screen)
+        show_stats(player)
 
-    if stage == END:
+    if player.level == 2:
+        screen.blit(background2, (0, 0))
+        lasers.draw(screen)
+        player.draw(screen)
+        bombs.draw(screen)
+        mobs.draw(screen)
+        show_stats(player)
+
+    if stage == LOSE:
         screen.blit(game_over, (0, 0))
+
+    if stage == WIN:
+        pygame.mixer.music.stop()
+        screen.blit(win_img, (0, 0))
+
 
 
     
